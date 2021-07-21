ebe95c03802b44a59ca9a60f1a695433065608d6
Ismail Bekirov <ismailbekirov99@gmail.com>
2021-07-21 13:19:58 +0300

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      PropertyCardComponent
+   ],
   imports: [
     BrowserModule
   ],
